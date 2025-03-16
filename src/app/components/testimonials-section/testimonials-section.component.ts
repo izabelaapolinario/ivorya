80acb1fc8250a0fe2beae1ce84cee9ad66060165
Izabela Apolinário <apolinarioizabela65@gmail.com>
2025-03-16 19:41:15 -0300

    alterações do header e estilização

--- src/app/components/testimonials-section/testimonials-section.component.ts
+++ src/app/components/testimonials-section/testimonials-section.component.ts
@@ -5,38 +5,44 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './testimonials-section.component.html',
   styleUrls: ['./testimonials-section.component.css']
 })
-export class TestimonialsSectionComponent implements OnInit{
-  currentIndex = 0;
-  testimonials = [
+export class TestimonialsSectionComponent implements OnInit {
+  currentIndex: number = 0;
+  testimonials: { image: string; name: string; position: string; feedback: string; }[] = [
+    {
+      image: 'assets/img/testimonials/testimonials-5.jpg',
+      name: 'Carlos Henrique',
+      position: 'CEO de uma empresa de tecnologia',
+      feedback: 'A Ivorya Soluções desenvolveu uma plataforma inovadora que realmente transformou nossa empresa. A abordagem personalizada e o compromisso com resultados concretos foram essenciais para nosso sucesso.'
+    },
     {
       image: 'assets/img/testimonials/testimonials-1.jpg',
-      name: 'Carlos Henrique',
-      position: 'CEO da TechSolvers',
-      feedback: 'A Ivorya Soluções desenvolveu uma plataforma inovadora para a nossa empresa. Sua abordagem personalizada trouxe resultados concretos e impactantes.'
-    },
-    {
-      image: 'assets/img/testimonials/testimonials-2.jpg',
       name: 'Fernanda Oliveira',
-      position: 'Gerente de Projetos - InovaSoft',
-      feedback: 'A equipe da Ivorya Soluções foi essencial na transformação digital da nossa empresa. Cada fase do projeto foi conduzida com excelência e dedicação.'
+      position: 'Gerente de Projetos em uma empresa de software',
+      feedback: 'A equipe da Ivorya Soluções foi fundamental na nossa transformação digital. Cada fase do projeto foi conduzida com excelência e dedicação, superando nossas expectativas.'
     },
     {
-      image: 'assets/img/testimonials/testimonials-3.jpg',
-      name: 'Rafael Santos',
-      position: 'CTO da NextGen Solutions',
-      feedback: 'A expertise da Ivorya Soluções no desenvolvimento de soluções escaláveis ajudou a nossa startup a alcançar novos patamares em termos de performance.'
+      image: 'assets/img/testimonials/testimonials-6.jpg',
+      name: 'Rafaela Santos',
+      position: 'CTO de uma startup',
+      feedback: 'A expertise da Ivorya Soluções no desenvolvimento de soluções escaláveis foi crucial para nossa startup. Graças a eles, alcançamos novos patamares de performance e eficiência.'
     },
     {
       image: 'assets/img/testimonials/testimonials-4.jpg',
       name: 'Amanda Souza',
-      position: 'Fundadora da DigitalBoost',
-      feedback: 'O trabalho da Ivorya Soluções foi fundamental para otimizar nossos processos internos. A entrega foi feita dentro do prazo e com altíssima qualidade.'
+      position: 'Fundadora de uma empresa de marketing digital',
+      feedback: 'O trabalho da Ivorya Soluções foi essencial para otimizar nossos processos internos. A entrega foi feita dentro do prazo e com altíssima qualidade, impactando positivamente nossos resultados.'
     },
     {
-      image: 'assets/img/testimonials/testimonials-5.jpg',
+      image: 'assets/img/testimonials/testimonials-3.jpg',
       name: 'Eduardo Lima',
       position: 'Empreendedor e Consultor de TI',
-      feedback: 'A Ivorya Soluções transformou nossa plataforma com uma solução personalizada e eficiente, o que gerou um impacto significativo nos nossos resultados.'
+      feedback: 'A Ivorya Soluções transformou nossa plataforma com uma solução personalizada e eficiente. O impacto nos nossos resultados foi significativo e imediato.'
+    },
+    {
+      image: 'assets/img/testimonials/testimonials-2.png',
+      name: 'Mariana Silva',
+      position: 'Coordenadora de uma instituição assistencial',
+      feedback: 'A Ivorya Soluções foi essencial para o desenvolvimento de nossa plataforma de gestão. A equipe demonstrou um compromisso excepcional com nossas necessidades, resultando em uma solução que realmente fez a diferença para nossa instituição.'
     }
   ];
 
@@ -46,9 +52,39 @@ export class TestimonialsSectionComponent implements OnInit{
     setInterval(() => {
       this.changeTestimonial();
     }, 5000);
+
+    document.addEventListener('DOMContentLoaded', () => {
+      if (this.testimonials.length > 0) {
+        this.showTestimonial(this.currentIndex);
+      }
+    });
   }
 
   changeTestimonial(): void {
     this.currentIndex = (this.currentIndex + 1) % this.testimonials.length;
+    this.showTestimonial(this.currentIndex);
+  }
+
+  showTestimonial(index: number): void {
+    const testimonialItem = document.querySelector('.testimonial-item') as HTMLElement;
+    const testimonialImg = testimonialItem.querySelector('.testimonial-img') as HTMLImageElement;
+    const testimonialName = testimonialItem.querySelector('h3') as HTMLElement;
+    const testimonialPosition = testimonialItem.querySelector('h4') as HTMLElement;
+    const testimonialFeedback = testimonialItem.querySelector('span') as HTMLElement;
+
+    testimonialImg.src = this.testimonials[index].image;
+    testimonialName.textContent = this.testimonials[index].name;
+    testimonialPosition.textContent = this.testimonials[index].position;
+    testimonialFeedback.textContent = this.testimonials[index].feedback;
+  }
+
+  prevTestimonial(): void {
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.testimonials.length - 1;
+    this.showTestimonial(this.currentIndex);
+  }
+
+  nextTestimonial(): void {
+    this.currentIndex = (this.currentIndex < this.testimonials.length - 1) ? this.currentIndex + 1 : 0;
+    this.showTestimonial(this.currentIndex);
   }
 }
